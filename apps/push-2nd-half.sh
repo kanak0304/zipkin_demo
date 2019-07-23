9f51aa1bf954038033121c7641616a665f0288ce
Kan Nakamura <nakamk2@KANAKs-Mac-Book-Pro.local>
2019-07-23 12:51:36 +0900

    clean up scripts.

--- apps/push-2nd-half.sh
+++ apps/push-2nd-half.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 
-
-#for f in $(find . -name pom.xml);do
-# pushd $(dirname $f);
-# ./mvnw clean package -DskipTests=true
-# popd
-#done
-
 for f in $(find . -name manifest.yml| tail -4);do
  pushd $(dirname $f);
  cf push -f $f;
